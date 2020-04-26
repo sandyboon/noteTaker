8b093d1525658a15c4c4a8f5bf9837373d552f29
ssingh <sandy.boon@gmail.com>
2020-04-26 17:28:54 -0400

    Added comments

--- server.js
+++ server.js
@@ -6,10 +6,12 @@ const mainRouter = require('./controllers/mainRouter');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
+//all the logic for Notes CRUD is in the NoteTaker module.
+//mainRouter is the parent router which defines routes for html and public assets.
+//The mainRouter imports the apiRouter which handles routing for api calls.
 app.use(mainRouter);
 
 //start the server
-
 app.listen(PORT, () => {
   console.log('Server is listning on port : ' + PORT);
 });
