0a3d462618ee4e64f5c516503954a3ec45f3dfca
ssingh <sandy.boon@gmail.com>
2020-04-26 01:14:01 -0400

    moved routers to controllers package

--- apiRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const moment = require('moment');
-const path = require('path');
-const apiRouter = express.Router();
-
-apiRouter.use(express.urlencoded({ extended: true }));
-apiRouter.use(express.json());
-
-apiRouter.get('/api/notes', function (req, res) {});
